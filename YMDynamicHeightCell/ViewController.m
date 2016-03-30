6aee1afd6c5e13d1007ccada9b93fc325dbf6093
iYiming <iYiming@163.com>
2016-03-30 15:47:16 +0800

    修改注释

--- YMDynamicHeightCell/ViewController.m
+++ YMDynamicHeightCell/ViewController.m
@@ -22,6 +22,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // branch hello
     
     personsArray = [[NSMutableArray alloc] init];
     
